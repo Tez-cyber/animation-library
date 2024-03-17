bc65a8f58ddf0b638a32dc5f35801f9d3724fd92
Salami Azeez <salamitopeazeez@gmail.com>
2024-03-16 19:09:02 -0700

    Added animation to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import Content from './Content'
 import Header from './Header'
 import Images from './Images'
+import { gsap, Power3  } from "gsap"
 
 function App() {
+  let tl =new gsap.timeline();
+  let ease = Power3.easeOut()
 
   return (
     <>
       <div className="hero">
-        <Header />
+        <Header timeline={tl} ease={ease}  />
         <div className="flex items-center justify-between w-full h-screen flex-row">
           <Content />
           <Images />
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const Header = () => {
+const Header = ({timeline, ease}) => {
+    let logo = useRef(null)
+    let menu_item1 = useRef(null)
+    let menu_item2 = useRef(null)
+    let menu_item3 = useRef(null)
+    useEffect(() => {
+        timeline.from(logo, 1, {
+            opacity: 0,
+            y: 100
+        });
+        timeline.from([menu_item1, menu_item2, menu_item3], 2, {
+            opacity: 0,
+            y: -100,
+            stagger: {
+                amount: .4
+            },
+            ease: ease
+        })
+    })
   return (
     <div className=''>
         <div className="header absolute top-0 flex items-center justify-between h-[100px] w-full">
-            <div className="logo ml-[50px] text-2xl font-extrabold text-[#f0f0f0]">
+            <div
+                ref={el => logo = el}
+                className="logo ml-[50px] text-2xl font-extrabold text-[#f0f0f0]">
                 Logo
             </div>
             <div className="menu flex mr-10">
-                <div className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">About</div>
-                <div className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">Journal</div>
-                <div className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">Contact</div>
+                <div ref={el => menu_item1 = el} className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">About</div>
+                <div ref={el => menu_item2 = el} className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">Journal</div>
+                <div ref={el => menu_item3 = el} className="menu-item ml-5 text-base text-[#f0f0f0] font-extralight">Contact</div>
             </div>
         </div>
     </div>
